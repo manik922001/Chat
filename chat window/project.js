e50af84c3436f94b94fabf34ff46cf23076e2276
manik922001 <57584417+manik922001@users.noreply.github.com>
2022-03-02 14:34:54 +0530

    Add files via upload

---
+++ chat window/project.js
@@ -0,0 +1,43 @@
+
+function myfun(){
+	
+	document.getElementById("rahul").style.display='none';
+	document.getElementById("parth").style.display='none';
+	document.getElementById("internet").style.display='none';
+	document.getElementById("abc").style.display='none';
+	document.getElementById("manik").style.display='block';
+	
+	
+}
+
+
+function myfun1(){
+	document.getElementById("manik").style.display='none';
+		document.getElementById("internet").style.display='none';
+		document.getElementById("parth").style.display='none';
+	document.getElementById("abc").style.display='none';
+	document.getElementById("rahul").style.display='block';
+}
+
+
+function myfun2(){
+	document.getElementById("manik").style.display='none';
+		document.getElementById("rahul").style.display='none';
+	document.getElementById("internet").style.display='none';
+	document.getElementById("abc").style.display='none';
+	document.getElementById("parth").style.display='block';
+}
+function myfun3(){
+	document.getElementById("manik").style.display='none';
+		document.getElementById("rahul").style.display='none';
+	document.getElementById("parth").style.display='none';
+	document.getElementById("abc").style.display='none';
+	document.getElementById("internet").style.display='block';
+}
+function myfun4(){
+	document.getElementById("manik").style.display='none';
+		document.getElementById("rahul").style.display='none';
+		document.getElementById("internet").style.display='none';
+	document.getElementById("parth").style.display='none';
+	document.getElementById("abc").style.display='block';
+}
